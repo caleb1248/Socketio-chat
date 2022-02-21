ec320592b952831e7a7904ac6210b2a4c462b32c
Caleb <classicalcaleb@gmail.com>
2022-02-21 16:14:03 -0500

    the new and improved chat app

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const io = new Server(server);
 const uuid = require('uuid');
 
 app.get('/', (_req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.redirect(uuid.v4());
+});
+
+app.get("/:room", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', socket => {
-  console.log('a user connected');
 	socket.on('join', id => {
 		socket.join(id);
 		socket.on('message', message => {
-			io.to(id).emit(message);
+			io.to(id).emit("message", message);
 		});
 	})
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on http://localhost:3000');
 });
